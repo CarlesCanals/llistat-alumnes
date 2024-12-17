91c26f8fdf753f49574e9d07034bff6ceea17d3b
Carles Canals <cgc@paucasesnovescifp.cat>
2024-12-17 16:16:24 +0100

    Afegeix suport per a la renderització del servidor i millora la configuració de HttpClient

--- src/main.ts
+++ src/main.ts
@@ -1,12 +1,12 @@
 import { bootstrapApplication } from '@angular/platform-browser';
-import { AppComponent } from './app/app.component';
 import { provideHttpClient, withFetch } from '@angular/common/http';
+import { AppComponent } from './app/app.component';
 import { provideRouter } from '@angular/router';
 import { routes } from './app/app.routes';
 
 bootstrapApplication(AppComponent, {
   providers: [
-    provideRouter(routes), // Rutes de l'aplicació
-    provideHttpClient(withFetch()) // Activa l'ús de Fetch API
+    provideHttpClient(withFetch()), // Configura HttpClient amb fetch APIs
+    provideRouter(routes)
   ]
 }).catch(err => console.error(err));
