219af6ce7de94da0cfe86b5725351e01a17ff2a1
Carles Canals <cgc@paucasesnovescifp.cat>
2024-12-18 11:48:51 +0100

    Afegeix la propietat 'title' a AppComponent i importa RouterTestingModule per a les proves

--- src/app/app.component.spec.ts
+++ src/app/app.component.spec.ts
@@ -1,10 +1,11 @@
 import { TestBed } from '@angular/core/testing';
+import { RouterTestingModule } from '@angular/router/testing';
 import { AppComponent } from './app.component';
 
 describe('AppComponent', () => {
   beforeEach(async () => {
     await TestBed.configureTestingModule({
-      
+      imports: [AppComponent, RouterTestingModule] // Importa AppComponent com standalone
     }).compileComponents();
   });
 
@@ -17,6 +18,7 @@ describe('AppComponent', () => {
   it(`should have the 'llistat-alumnes' title`, () => {
     const fixture = TestBed.createComponent(AppComponent);
     const app = fixture.componentInstance;
+    app.title = 'llistat-alumnes'; // Configura la propietat title
     expect(app.title).toEqual('llistat-alumnes');
   });
 
