35be9313c11b04fc7342e93c3667697122f544c2
Carles Canals <cgc@paucasesnovescifp.cat>
2024-12-18 11:28:40 +0100

    Afegeix fitxers i rutes per al desplegament a la carpeta 'llistat-alumnes'

--- dist/llistat-alumnes/browser/server/assets-chunks/index_csr_html.mjs
+++ dist/llistat-alumnes/browser/server/assets-chunks/index_csr_html.mjs
@@ -1,4 +1,4 @@
-<!doctype html>
+export default `<!doctype html>
 <html lang="en" data-beasties-container="">
 <head>
   <meta charset="utf-8">
@@ -9,5 +9,6 @@
 <link rel="stylesheet" href="styles-5INURTSO.css"></head>
 <body ngcm="">
   <app-root></app-root>
-<script src="polyfills-FFHMD2TL.js" type="module"></script><script src="main-5YT5V2LH.js" type="module"></script></body>
+<script src="polyfills-FFHMD2TL.js" type="module"></script><script src="main-QJTG53NX.js" type="module"></script></body>
 </html>
+`;
