5707356b2685fed0b418786cc6727e3aba6e3e94
Carles Canals <cgc@paucasesnovescifp.cat>
2024-12-19 07:18:01 +0100

    Actualitza els noms dels scripts en els fitxers HTML i el manifest d'actius

--- dist/llistat-alumnes/browser/server/angular-app-manifest.mjs
+++ dist/llistat-alumnes/browser/server/angular-app-manifest.mjs
@@ -23,12 +23,12 @@ export default {
   }
 ],
   assets: {
-    'index.csr.html': {size: 514, hash: 'd5fee37d9a9b696e1426b85929b2b49ff096ae041587168ef252be8d6cee4383', text: () => import('./assets-chunks/index_csr_html.mjs').then(m => m.default)},
-    'index.server.html': {size: 1027, hash: '851865afaa6b08ef105890b977cccebcf61a4d5702b7d93018bf54450a561758', text: () => import('./assets-chunks/index_server_html.mjs').then(m => m.default)},
-    'llistat/index.html': {size: 3488, hash: '3403581586a03f1c0bbee163a371f974d93ea833ac04f9bb25a65e817c2b75f0', text: () => import('./assets-chunks/llistat_index_html.mjs').then(m => m.default)},
-    'notes/index.html': {size: 3527, hash: '8d13e220f5e331dedaf0f3b15da8fc9535478d01c5736844e9e189729a51d30f', text: () => import('./assets-chunks/notes_index_html.mjs').then(m => m.default)},
-    'calendari/index.html': {size: 4259, hash: '7902be4d7b9892df0fb162c24d422eefa2050b9997bc47fdd1f1fc4c56c2549d', text: () => import('./assets-chunks/calendari_index_html.mjs').then(m => m.default)},
-    'index.html': {size: 6614, hash: '6486787649097edb0fcbfc2063a20c59d19767e9d8395e944d9cca1d352a74a8', text: () => import('./assets-chunks/index_html.mjs').then(m => m.default)},
+    'index.csr.html': {size: 514, hash: '2be9f93280b796cf262907f2388d7b74ca16ef4ebbb4127235d623c2f1e448a5', text: () => import('./assets-chunks/index_csr_html.mjs').then(m => m.default)},
+    'index.server.html': {size: 1027, hash: '56f6c7b327c77fd885186d40b2cbadc236ff561a9039ac1c4bb2444ce5853791', text: () => import('./assets-chunks/index_server_html.mjs').then(m => m.default)},
+    'index.html': {size: 6614, hash: '791c43a69cd2ca00a45514eaa52c193b68897fe8fa2fc612dbbf446ecdf3ab9a', text: () => import('./assets-chunks/index_html.mjs').then(m => m.default)},
+    'llistat/index.html': {size: 3488, hash: '3c046dd5953f2321fcc81b1bb41ebc0fd3518f465b8c1fb8389f67d84715f013', text: () => import('./assets-chunks/llistat_index_html.mjs').then(m => m.default)},
+    'calendari/index.html': {size: 4259, hash: '1e7845864454b71bfdcda05e9bef7eccd10306c980a8317823fb2772f1fe97b2', text: () => import('./assets-chunks/calendari_index_html.mjs').then(m => m.default)},
+    'notes/index.html': {size: 3527, hash: 'ae58653f7e860595cbd6a7a5bc17139e636b95861b8077b666e8329c435afb4c', text: () => import('./assets-chunks/notes_index_html.mjs').then(m => m.default)},
     'styles-5INURTSO.css': {size: 0, hash: 'menYUTfbRu8', text: () => import('./assets-chunks/styles-5INURTSO_css.mjs').then(m => m.default)}
   },
 };
